1b9e7038c0f0198ac54dba2b64b94790a16450a3
parmarraj999 <rajraja5122@gmail.com>
2025-03-07 15:17:17 +0530

    done

--- src/sections/instagram-section/instagram.js
+++ src/sections/instagram-section/instagram.js
@@ -6,10 +6,44 @@ function Instagram() {
     <div className='instagram-section' >
         <div className='instagram-wrapper' >
             <div className='card' >
-                
+                <div className='card-header'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <img src='../../../../images/instagram-img1.png' />
+                <div className='card-details' >
+                    <h2>Healthy Chickens</h2>
+                    <p>$22.00</p>
+                </div>
             </div>
-            <div className='card' ></div>
-            <div className='card' ></div>
+           
+            <div className='card' >
+                <div className='card-header'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <img src='../../../../images/instagram-img2.jpeg' />
+                <div className='card-details' >
+                    <h2>Vegan Salad</h2>
+                    <p>$18.20</p>
+                </div>
+            </div>
+           
+            <div className='card' >
+                <div className='card-header'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <img src='../../../../images/instagram-img3.png' />
+                <div className='card-details' >
+                    <h2>Ittalian Pizza</h2>
+                    <p>$12.75</p>
+                </div>
+            </div>
+           
         </div>
     </div>
   )
